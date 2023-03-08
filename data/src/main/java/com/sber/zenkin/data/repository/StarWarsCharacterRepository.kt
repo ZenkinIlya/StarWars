026c3177d6c0b2543d2638c707bdbc8a5394b2a4
ZenkinIlya <ilyazenkinn@mail.ru>
2023-03-08 14:56:11 +0300

    add to getCharactersFromDatabase variation get characters by name, add logging

--- data/src/main/java/com/sber/zenkin/data/repository/StarWarsCharacterRepository.kt
+++ data/src/main/java/com/sber/zenkin/data/repository/StarWarsCharacterRepository.kt
@@ -13,6 +13,7 @@ import com.sber.zenkin.domain.StarWarsRepository
 import com.sber.zenkin.domain.model.Character
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
+import timber.log.Timber
 import javax.inject.Inject
 
 class StarWarsCharacterRepository @Inject constructor(
@@ -23,44 +24,52 @@ class StarWarsCharacterRepository @Inject constructor(
 
     override suspend fun getCharactersFromApi(searchName: String): Flow<PagingData<Character>> {
         return Pager(
-            PagingConfig(pageSize = 10, initialLoadSize = 10, enablePlaceholders = true, prefetchDistance = 3),
+            PagingConfig(
+                pageSize = 10,
+                initialLoadSize = 10,
+                enablePlaceholders = true,
+                prefetchDistance = 3
+            ),
             pagingSourceFactory = { CharacterPagingSource(starWarsApiService, searchName) }
         ).flow
             .catch { }
     }
 
     //DataBase
-    override suspend fun getCharactersFromDb(searchName: String): List<Character> {
-        return try {
+    override fun getCharactersFromDatabase(searchName: String): List<Character> {
+        return if (searchName.isEmpty()) {
             appDatabase.charactersDao().getCharacters()
-                .map { it.toDomainCharacter() }
-        } catch (e: Exception) {
-            return emptyList()
+        } else {
+            appDatabase.charactersDao().getCharactersByName(searchName)
         }
+            .map { it.fromDomainCharacter() }
+            .onEach { Timber.d("characters from database: $it") }
     }
 
-    override suspend fun saveCharacterInDb(character: Character) {
-        appDatabase.charactersDao().insertCharacter(character.fromDomainCharacter())
+    override suspend fun saveCharacterInDatabase(character: Character) {
+        Timber.i("save character in database: ${character.name}")
+        appDatabase.charactersDao().insertCharacter(character.toDomainCharacter())
     }
 
-    override suspend fun deleteCharacterFromDb(character: Character) {
-        appDatabase.charactersDao().deleteCharacter(character.fromDomainCharacter())
+    override suspend fun deleteCharacterFromDatabase(character: Character) {
+        Timber.i("delete character from database: ${character.name}")
+        appDatabase.charactersDao().deleteCharacter(character.toDomainCharacter())
     }
 
-    override suspend fun deleteCharactersInDb() {
+    override suspend fun deleteAllCharactersFromDatabase() {
+        Timber.i("delete all characters from database")
         appDatabase.charactersDao().deleteAllCharacters()
     }
 
     //Cache
     override suspend fun saveCharacterInCache(character: Character) {
+        Timber.i("save character in cache: $character")
         charactersCache.putCharacter(character)
     }
 
     override suspend fun getCharactersFromCache(): List<Character> {
-        return try {
-            charactersCache.getCharacters()
-        } catch (e: Exception) {
-            emptyList()
-        }
+        val characters = charactersCache.getCharacters()
+        Timber.i("characters from cache: $characters")
+        return characters
     }
 }
