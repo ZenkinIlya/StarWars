f1badf7667e03c1f0d22bb55f5e010c57073d6f6
ZenkinIlya <ilyazenkinn@mail.ru>
2023-03-08 15:42:47 +0300

    get data in async scope

--- domain/src/main/java/com/sber/zenkin/domain/useCases/GetStarWarsCharacterUseCase.kt
+++ domain/src/main/java/com/sber/zenkin/domain/useCases/GetStarWarsCharacterUseCase.kt
@@ -4,39 +4,49 @@ import androidx.paging.PagingData
 import androidx.paging.map
 import com.sber.zenkin.domain.StarWarsRepository
 import com.sber.zenkin.domain.model.Character
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 
 class GetStarWarsCharacterUseCase(
     private val starWarsRepository: StarWarsRepository
 ) {
 
-    suspend fun getCharacters(searchName: String = "a"): Flow<PagingData<Character>> {
-        return starWarsRepository.getCharactersFromApi(searchName)
-            .map { pagingData ->
-                pagingData.map { character ->
-                    markFavoriteCharacter(
-                        character,
-                        searchName
-                    )
-                }
+    suspend fun getCharacters(searchName: String): Flow<PagingData<Character>> {
+        val deferredCharactersFromApi = coroutineScope {
+            async(Dispatchers.IO) {
+                starWarsRepository.getCharactersFromApi(searchName)
             }
-            .onEach { pagingData -> pagingData.map { starWarsRepository.saveCharacterInCache(it) } }
-            .catch { }
+        }
+        val deferredCharactersFromDb = coroutineScope {
+            async(Dispatchers.IO) {
+                getCharactersFromDb(searchName)
+            }
+        }
+
+        val pagingNetworkCharactersFlow = deferredCharactersFromApi.await()
+        val characterListDatabase = deferredCharactersFromDb.await()
+
+        return pagingNetworkCharactersFlow.map { pagingData ->
+            pagingData.map { character ->
+                markFavoriteCharacter(
+                    character, characterListDatabase
+                )
+            }
+        }.catch { }
     }
 
-    private suspend fun markFavoriteCharacter(
-        charactersFromData: Character,
-        searchName: String
+    private fun markFavoriteCharacter(
+        charactersFromData: Character, charactersFromDb: List<Character>
     ): Character {
-        val charactersFromDb = starWarsRepository.getCharactersFromDb(searchName)
         charactersFromData.favorite = charactersFromDb.contains(charactersFromData)
         return charactersFromData
     }
 
-    suspend fun getCharactersFromDb(searchName: String): List<Character> {
-        return starWarsRepository.getCharactersFromDb(searchName)
+    fun getCharactersFromDb(searchName: String): List<Character> {
+        return starWarsRepository.getCharactersFromDatabase(searchName)
     }
 }
